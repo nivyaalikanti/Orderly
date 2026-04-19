213de9102bff18f7e031b59b370b1a970b03d941
Alikanti Nivya <nivyaalikanti@gmail.com>
2026-04-19 22:41:06 +0530

    created restaurant Action

--- frontend/src/redux/actions/restaurantAction.js
+++ frontend/src/redux/actions/restaurantAction.js
@@ -17,3 +17,38 @@ export const getRestaurants = createAsyncThunk(
             return rejectWithValue(error.response.data.message || "Failed to fetch restaurants");
         }
     });
+
+// create restaurant - admin
+export const createRestaurant = createAsyncThunk(
+    "restaurants/createRestaurant",
+    async (restaurantData, { rejectWithValue }) => {
+        try {
+            const { data } = await api.post("/eats/stores", restaurantData, {
+                headers: { "Content-Type": "application/json" },
+            });
+            return data.data;
+        }
+        catch (error) {
+            return rejectWithValue(error.response.data.message || "Failed to create restaurant");
+        }
+    }
+);
+
+//delete restaurant - admin
+export const deleteRestaurant = createAsyncThunk(
+    "restaurants/deleteRestaurant",
+    async (id, { rejectWithValue }) => {
+        try {
+            await api.delete(`/eats/stores/${id}`);
+            return {
+                id,
+                message: "data.message",
+            };
+        }
+            
+        
+        catch (error) {
+            return rejectWithValue(error.response.data.message || "Failed to delete restaurant");
+        }
+    }
+);

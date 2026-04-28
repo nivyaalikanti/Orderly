89d6d1cfabad819735eeb04b7bad80a554ee6d12
Alikanti Nivya <nivyaalikanti@gmail.com>
2026-04-28 20:50:16 +0530

    completed redux store component

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import restaurantReducer from "./slices/restaurantSlice";
+import menuReducer from "./slices/menuSlice";
+import userReducer from "./slices/userSlice";
+import cartReducer from "./slices/cartSlice";
+import ordersReducer from "./slices/orderSlice";
 
 const store = configureStore({
     reducer: {
         restaurants: restaurantReducer,
+        menus: menuReducer,
+        user: userReducer,
+        cart: cartReducer,
+        orders: ordersReducer,
     },
 });
 
